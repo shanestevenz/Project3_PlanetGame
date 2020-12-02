c8d5bcb59b74bdf0ac2c51bfd80dc7c84e26a3cb
shane <shanestevenz@gmail.com>
2020-12-02 18:31:52 -0500

    Add project files.

---
+++ Project3_PlanetGame/game.cpp
@@ -0,0 +1,35 @@
+#pragma region Includes
+
+//Managers
+
+
+//Objects
+
+#pragma endregion
+
+// Function prototypes.
+void loadResources(void);
+void populateWorld(void);
+
+
+int main(int argc, char* argv[])
+{
+
+
+	//game manager
+
+	return 0;
+}
+
+
+// Load resources (sprites, sound effects, music).
+void loadResources(void) {
+   //rm load stuff
+}
+
+// Populate world with some objects.
+void populateWorld(void) {
+
+
+   //create objects
+}
