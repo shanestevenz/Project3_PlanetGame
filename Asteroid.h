07fe470fc365c592902ad4b0d1ddeaba66242ada
carrier-of-cyberbully <75399499+carrier-of-cyberbully@users.noreply.github.com>
2020-12-05 09:58:36 -0500

    Add files via upload

---
+++ Asteroid.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Object.h"
+#include "EventCollision.h"
+#include "WorldManager.h"
+
+
+class Asteroid : public df::Object {
+
+public:
+	Asteroid();
+	~Asteroid();
+	int randomIndex;
+
+	int eventHandler(const df::Event* p_e);
+	void out();
+	void hit(const df::EventCollision* p_c);
+	void moveToStart();
+	
+};
